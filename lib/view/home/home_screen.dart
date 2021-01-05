df363eebf25d232a36d0698cec2d45753943f33e
waterbook59 <takeshi.m64@gmail.com>
2021-01-05 20:33:10 +0900

    セクション5:瞑想画面 chapter42-44 Widget分割とSelector書き方

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:meditaition/data_models/level.dart';
 import 'package:meditaition/data_models/meiso_theme.dart';
 import 'package:meditaition/data_models/meiso_time.dart';
+import 'package:meditaition/data_models/user_settings.dart';
 import 'package:meditaition/generated/l10n.dart';
+import 'package:meditaition/view/home/components/decorated_background.dart';
+import 'package:meditaition/view/home/components/header_part.dart';
+import 'package:meditaition/view/home/components/play_button_part.dart';
+import 'package:meditaition/view/home/components/speed_dial_part.dart';
+import 'package:meditaition/view/home/components/status_diplay_part.dart';
+import 'package:meditaition/view/home/components/volume_slider_part.dart';
 import 'package:meditaition/view_models/main_view_model.dart';
 import 'package:provider/provider.dart';
 
-List<Level> levels =<Level>[];
-List<MeisoTheme> meisoThemes =<MeisoTheme>[];
-List<MeisoTime> meisoTimes =<MeisoTime>[];
+List<Level> levels = <Level>[];
+List<MeisoTheme> meisoThemes = <MeisoTheme>[];
+List<MeisoTime> meisoTimes = <MeisoTime>[];
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -19,17 +26,47 @@ class HomeScreen extends StatelessWidget {
 
     ///ユーザー設定をsharedPreferencesから取ってくる
 //    final viewModel = Provider.of<MainViewModel>(context,listen:false);
-    Future<dynamic>((){
+    Future<dynamic>(() {
       final viewModel = context.read<MainViewModel>();
       return viewModel.getUserSettings();
     });
 
-    return Scaffold(
-      body: Container(
-        child: Center(child: Text(S.of(context).introTitle1)),
-      ),
+    return SafeArea(
+      child: Scaffold(
+          //todo
+          floatingActionButton: SpeedDialPart(),
+          body: Selector<MainViewModel, UserSettings>(
+            selector: (context, viewModel) => viewModel.userSettings,
+            builder: (context, userSettings, child) {
+              return userSettings == null
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Stack(
+                      children: [
+                        //1階
+                        //todo グラデーション付背景画像
+                        DecoratedBackground(),
+                        //2階
+                        Column(
+                          children: [
+                            //todo
+                            HeaderPart(),
+                            //todo
+                            StatusDisplayPart(),
+                            //todo
+                            PlayButtonPart(),
+                            //todo
+                            VolumeSliderPart(),
+                          ],
+                        )
+                      ],
+                    );
+            },
+          )),
     );
   }
+
   List<Level> setLevels(BuildContext context) {
     return [
       Level(
