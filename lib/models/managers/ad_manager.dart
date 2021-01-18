f85b38662f9632c9cefb5665d174afa678793224
waterbook59 <takeshi.m64@gmail.com>
2021-01-18 17:44:21 +0900

    ad_manager設定

--- lib/models/managers/ad_manager.dart
+++ lib/models/managers/ad_manager.dart
@@ -1,5 +1,38 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class AdManager{
-  
+
+  static String get appId {
+    if (Platform.isAndroid) {
+      return "<YOUR_ANDROID_ADMOB_APP_ID>";
+    } else if (Platform.isIOS) {
+      return "<YOUR_IOS_ADMOB_APP_ID>";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return "<YOUR_ANDROID_BANNER_AD_UNIT_ID";
+    } else if (Platform.isIOS) {
+      return "<YOUR_IOS_BANNER_AD_UNIT_ID>";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>";
+    } else if (Platform.isIOS) {
+      return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
 }
